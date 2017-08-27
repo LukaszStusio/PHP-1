825bd4ef2ab9f96d69feeb034c9af8488b250044
LukaszStusio <lukasz.stusio@gmail.com>
2017-08-27 15:18:43 +0200

    set up

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+
+<html lang="pl">
+
+<head>
+	<meta charset="utf-8" />
+
+	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<title>Cukiernia "Okruszki" w Poznaniu</title> 
+	<meta name="description" content="Wyśmienite pączki są naszą specjalnością." />
+	<meta name="keywords" content="" />
+	
+</head>
+<body>
+
+	<h1>Zamówienie online</h1>
+	<form action="order.php" method="post">
+		Ile pączków zamawiasz? (0,99zł/szt.):
+		<input type="text" name="paczkow"/>
+		<br />
+		<br />
+		Ile grzebieni zamawiasz? (1,29zł/szt.):
+		<input type="text" nme="grzebieni"/>
+		<br />
+		<br />
+		<input type="submit" value="Wyślij zamówienie" />
+	</form>
+
+</body>
+</html>
---
+++ order.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+
+<html lang="pl">
+
+<head>
+	<meta charset="utf-8" />
+
+	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<title>Podsumowanie zamówienia</title> 
+	<meta name="description" content="Wyśmienite pączki są naszą specjalnością." />
+	<meta name="keywords" content="" />
+	
+</head>
+<body>
+
+	
+
+</body>
+</html>
